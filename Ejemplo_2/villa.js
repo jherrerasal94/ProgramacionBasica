159014c18bb47c4962a9268753562970375f319b
jherrerasal94 <j.ahsjesus1002@gmail.com>
2019-05-22 21:59:44 -0500

    Se crean comentarios y ademas se deja funcional para carguede animales con ubicacion aleatoria

--- Ejemplo_2/villa.js
+++ Ejemplo_2/villa.js
@@ -1,21 +1,51 @@
+/**
+ * @fileoverview Canvas con una granja de animales
+ * @version 1.0
+ * @author Jesús Herrera
+ * @copyright Platzi.com
+*/
 
-
-for(var i = 0; i <= 20; i++){
-    console.log(obtenerAleatorio(0, 20));
-}
-
-var marco = document.getElementById("villaPC");
+var marco = document.getElementById("marcoTrabajo");
 var papel = marco.getContext("2d");
-var mapa = "tile.png";
 
-var imagen = new Image();
-imagen.src = mapa;
-imagen.addEventListener("load", dibujar);
+//Animales disponibles
+var mapa = "tile.png";
+var pollo = "pollo.png";
+var cerdo = "cerdo.png";
+var vaca = "vaca.png";
+
+//Mapa
+var imgMapa = new Image(); 
+imgMapa.src = mapa;
+imgMapa.addEventListener("load", dibujar);
+
+//Vaca
+var imgVaca = new Image(); 
+imgVaca.src = vaca;
+imgVaca.addEventListener("load", dibujar);
+
+//Pollo
+var imgPollo = new Image(); 
+imgPollo.src = pollo;
+imgPollo.addEventListener("load", dibujar);
+
+//Cerdo
+var imgCerdo = new Image(); 
+imgCerdo.src = cerdo;
+imgCerdo.addEventListener("load", dibujar);
+
 
 function dibujar(){
-    papel.drawImage(imagen, 0, 0);
+    papel.drawImage(imgMapa, 0, 0);
+    papel.drawImage(imgPollo, (obtenerAleatorio(0, 6) * 40), (obtenerAleatorio(0, 6) * 40));
+    papel.drawImage(imgCerdo, (obtenerAleatorio(0, 6) * 40), (obtenerAleatorio(0, 6) * 40));
+    papel.drawImage(imgVaca, (obtenerAleatorio(0, 6) * 40), (obtenerAleatorio(0, 6) * 40));
 }
-
+/**
+ * Método que devuelve un número al azar entre un rango
+ * @param {*} numMin valor mínimo
+ * @param {*} numMax valor máximo
+ */
 function obtenerAleatorio(numMin, numMax){
     var resultado;
     resultado = Math.floor(Math.random() * (numMax - numMin + 1)) + numMin;
